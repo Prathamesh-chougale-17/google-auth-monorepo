da41e9f90b1ccb75047a2613b3355a3388624fba
Prathamesh Chougale <prathamesh17170@gmail.com>
2026-03-13 00:25:49 +0530

    Fix turborepo env type errors

--- packages/api/src/routers/index.ts
+++ packages/api/src/routers/index.ts
@@ -1,6 +1,6 @@
 import type { RouterClient } from "@orpc/server";
 
-import { protectedProcedure, publicProcedure } from "../index";
+import { protectedProcedure, publicProcedure } from "../index.js";
 
 export const appRouter = {
   healthCheck: publicProcedure.handler(() => {
