f6c4a74ba796d0337b9b81e5d5a78dd0dbd3d951
Prathamesh Chougale <prathamesh17170@gmail.com>
2026-03-12 22:34:46 +0530

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,29 @@
+import { expo } from "@better-auth/expo";
+import { client } from "@tinywins/db";
+import { env } from "@tinywins/env/server";
+import { betterAuth } from "better-auth";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+export const auth = betterAuth({
+  database: mongodbAdapter(client),
+  trustedOrigins: [
+    env.CORS_ORIGIN,
+    "tinywins://",
+    ...(env.NODE_ENV === "development"
+      ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
+      : []),
+  ],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [expo()],
+});
